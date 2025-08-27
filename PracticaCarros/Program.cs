7756185c8929ed719d262b682add9d1d2a03e3f8
Mariana <2269068@gmail.com>
2025-08-27 18:26:50 -0500

    primer commit

--- PracticaCarros/Program.cs
+++ PracticaCarros/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("SISTEMA DE GESTION");
 
+var carro = new Carros();
+Carros carro1 = new Carros();
+Carros carro2 = new Deportivos();

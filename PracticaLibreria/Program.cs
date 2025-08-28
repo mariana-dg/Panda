6c732f778e48a1f42a89d2d0d22a88c4b1dce207
Mariana <2269068@gmail.com>
2025-08-27 20:42:00 -0500

    clases proyecto libros

---
+++ PracticaLibreria/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Generos
+{
+    public int Id = 0;
+    public string Nombre = "";
+}
+public class Libros
+{
+    public int Id = 0;
+    public string Titulo = "";
+    public DateTime FechaPublicacion = DateTime.Now;
+    
+}
